51207ea0ba37cdbac1dd2b2bffacbb6dbc79fc36
one <one@gmail.com>
2025-11-09 05:49:45 +0800

    1

--- database/seeds/CarrierUserTableSeeder.php
+++ database/seeds/CarrierUserTableSeeder.php
@@ -12,7 +12,8 @@ class CarrierUserTableSeeder extends Seeder
     public function run()
     {
         DB::table('inf_carrier_user')->insert([
-            'id'                => 1,
+            'id'                     => 1,
+            'team_id'                => 1,
             'username'               => 'winwinasia',
             'password'               => \Hash::make('e10adc3949ba59abbe56e057f20f883e'),
             'status'                 => 1,
