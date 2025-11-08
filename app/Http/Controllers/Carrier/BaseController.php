3c33a9f6b8d5805088ddef1ba7efbeb57a7be55b
one <one@gmail.com>
2025-11-09 03:51:18 +0800

    1

--- app/Http/Controllers/Carrier/BaseController.php
+++ app/Http/Controllers/Carrier/BaseController.php
@@ -12,12 +12,13 @@ class BaseController extends Controller
 
     public function __construct () 
     {
+        \Log::info('进入基本层');
         $tokenHeader = request()->header('Authorization');
         if(!empty($tokenHeader) && strpos($tokenHeader,'bearer')!==false) {
             $this->authToken = explode(' ', $tokenHeader)[1];
         }
 
-        $this->carrierUser = auth("carrier")->user();
+        //$this->carrierUser = auth("carrier")->user();
 
     }
 }
